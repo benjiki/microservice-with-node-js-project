1e929a5367520bb057948b00738d6e285c381e17
biniyam yoseph <biniyamyoseph063@gmail.com>
2025-10-16 21:32:24 +0300

    feat(auth-service): initialize auth service with Express, CORS, and Helmet
    - Added package.json with necessary dependencies and devDependencies.
    - Created .env.example for environment variable configuration.
    - Implemented basic Express server setup in index.ts with middleware for CORS and security.

---
+++ service/auth-service/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8001;
+
+//setup middleware
+app.use(cors());
+app.use(helmet());
+
+//  parse JSON bodies
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`Auth service is running on port ${PORT}`);
+  console.log(`Environment: ${process.env.NODE_ENV}`);
+  console.log(`Health check endpoint: http://localhost:${PORT}/health`);
+});
+
+export default app;
